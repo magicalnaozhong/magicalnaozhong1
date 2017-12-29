7fc093ccf922031b4a8b4a870275c3c3f6e4f293
crazy000 <34770852+crazy000@users.noreply.github.com>
2017-12-29 23:08:48 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+App({
+  onLaunch() {
+    console.log('App.onLaunch()');
+  },
+  onShow: function () {
+  },
+  onHide: function () {
+  },
+
+  //��Ӧ��ȫ������
+  globalData: {
+    temperature: {},
+    light: {},
+    humidity: {} 
+  }
+})

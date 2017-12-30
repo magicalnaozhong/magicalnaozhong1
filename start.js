5128655b6895817995bfaf5d4610ddca84f28afc
crazy000 <34770852+crazy000@users.noreply.github.com>
2017-12-31 00:43:37 +0800

    Update start.js

--- start.js
+++ start.js
@@ -3,7 +3,7 @@
 Page({
 
   /**
-   * ҳ��ĳ�ʼ����
+   * 页面的初始数据
    */
   data: {
     opacity: 0.4,
@@ -11,13 +11,21 @@ Page({
     threshold: 0,
     rule: 'up',
     items: [
-      { name: 'up', value: '������������' ,checked:'ture'},
-      { name: 'down', value: '������������' },
+      { name: 'up', value: '高于门限提醒' ,checked:'ture'},
+      { name: 'down', value: '低于门限提醒' },
     ]
   },
+plan:function(){
+  wx.showModal({
+    title: '备忘录',
+    content: '①下午去还书  ②晚上六点和家人通话'
+  })
+},
+
+
 
   radioChange: function (e) {
-    //���汨�����򵽵�ǰҳ�������
+    //保存报警规则到当前页面的数据
     if (e.detail.value != "") {
       this.setData({
         rule: e.detail.value
@@ -27,48 +35,50 @@ Page({
   },
 
   send: function(){
-    //�����������ݺͱ������򵽺�̨������
+    //发送门限数据和报警规则到后台服务器
     var thres = this.data.threshold
     var Rule = this.data.rule
     const requestTask = wx.request({
-      url: 'https://xxxxxxxx.xxxxxx.xx', //�ĳ���Ѷ�Ƹ�����������
+      url: 'https://xxxxxxxx.xxxxxx.xx 
+
+', //改成腾讯云给你分配的域名
       header: {
         'content-type': 'application/json',
       },
 
-      //���͸�������������
+      //发送给服务器的数据
       data:{
         threshold: thres,
         rule: Rule
       },
       success: function (res) {
-        //�ɹ��õ����������ص����ݺ�ִ���������
+        //成功拿到服务器返回的数据后执行这个函数
         var flag = res.data;
         console.log(flag)
-        //����1�򱨾�
+        //返回1则报警
         if(flag == "1"){
           wx.showModal({
-            title: '������',
-            content: '�¶ȹ���',
+            title: '警报！',
+            content: '温度过高',
             success: function (res) {
               if (res.confirm) {
-                console.log('�û����ȷ��')
+                console.log('用户点击确定')
               } else if (res.cancel) {
-                console.log('�û����ȡ��')
+                console.log('用户点击取消')
               }
             }
           })
         }
-        //����0�򲻱���
+        //返回0则不报警
         else if (flag == "0"){
           wx.showModal({
-            title: '��ʾ��',
-            content: '�¶ȴ���������Χ��',
+            title: '提示～',
+            content: '温度处于正常范围。',
             success: function (res) {
               if (res.confirm) {
-                console.log('�û����ȷ��')
+                console.log('用户点击确定')
               } else if (res.cancel) {
-                console.log('�û����ȡ��')
+                console.log('用户点击取消')
               }
             }
           })
@@ -86,22 +96,24 @@ Page({
   },
 
   getDataFromOneNet: function(){
-    //��oneNET�������ǵ�Wi-Fi����վ������
+    //从oneNET请求我们的Wi-Fi气象站的数据
     const requestTask = wx.request({
-      url: 'https://api.heclouds.com/devices/20470765/datapoints?datastream_id=Light,Temperature,Humidity&limit=50',
+      url: 'https://api.heclouds.com/devices/20470765/datapoints?datastream_id=Light,Temperature,Humidity&limit=50 
+
+',
       header: {
         'content-type': 'application/json',
         'api-key': 'EAXlG5aKHKM=H=j4PpO=cw530LY='
       },
       success: function (res) {
         //console.log(res.data)
-        //�õ����ݺ󱣴浽ȫ������
+        //拿到数据后保存到全局数据
         var app = getApp()
         app.globalData.temperature = res.data.data.datastreams[0]
         app.globalData.light = res.data.data.datastreams[1]
         app.globalData.humidity = res.data.data.datastreams[2]
         console.log(app.globalData.light)
-        //��ת������ҳ�棬�����õ������ݻ�ͼ
+        //跳转到天气页面，根据拿到的数据绘图
         wx.navigateTo({
           url: '../wifi_station/tianqi/tianqi',
         })
@@ -120,7 +132,7 @@ Page({
   },
 
   change: function (e) {
-    //��������ʱ����Ͱ�ť������������ð�ť
+    //当有输入时激活发送按钮，无输入则禁用按钮
     if (e.detail.value != "") {
       this.setData({
         threshold: e.detail.value,
@@ -137,56 +149,56 @@ Page({
   },
 
   /**
-   * �������ں���--����ҳ�����
+   * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
   
   },
 
   /**
-   * �������ں���--����ҳ�������Ⱦ���
+   * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
   
   },
 
   /**
-   * �������ں���--����ҳ����ʾ
+   * 生命周期函数--监听页面显示
    */
   onShow: function () {
   
   },
 
   /**
-   * �������ں���--����ҳ������
+   * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
   
   },
 
   /**
-   * �������ں���--����ҳ��ж��
+   * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
   
   },
 
   /**
-   * ҳ������¼���������--�����û���������
+   * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
   
   },
 
   /**
-   * ҳ�����������¼��Ĵ�������
+   * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
   
   },
 
   /**
-   * �û�������ϽǷ���
+   * 用户点击右上角分享
    */
   onShareAppMessage: function () {
   
